b9de9e6154c1a5768d2ece5247e2d70fcb5521eb
Mariana Lopez <mariana.lopez.it@gmail.com>
2022-04-25 18:48:51 -0300

    Add files via upload

---
+++ Javascript/Tabla_multiplicar.js
@@ -0,0 +1,26 @@
+
+	function saltarLinea(){
+		document.write("<br>");
+		document.write("<br>");
+		document.write("<br>");
+	}
+
+	function imprimir(frase){
+		document.write(frase);
+		saltarLinea();
+	}
+
+    //var multiplicador = 1;
+
+    /*while(multiplicador <= 10)
+    {
+        imprimir(5 * multiplicador);
+        multiplicador = multiplicador + 1;
+    }*/
+
+    imprimir("FOR");
+
+    for(var multiplicador = 1; multiplicador <= 10; multiplicador = multiplicador + 1)
+    {
+
+    }
