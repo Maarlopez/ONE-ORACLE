ad4149cb34a6c825bc3faf1938bf2ef170f59a15
Mariana Lopez <mariana.lopez.it@gmail.com>
2022-04-25 18:42:14 -0300

    Create JUEGO_ADIVINACION.js

---
+++ Javascript/JUEGO_ADIVINACION.js
@@ -0,0 +1,26 @@
+<script>
+
+	function saltarLinea(){
+		document.write("<br>");
+		document.write("<br>");
+		document.write("<br>");
+	}
+
+	function imprimir(frase){
+		document.write(frase);
+		saltarLinea();
+	}
+
+	var numeroPensado = Math.round(Math.random()*10);
+	var numeroLanzado = parseInt(prompt("Ingrese un número entre 0-10"));
+
+	if(numeroPensado == numeroPensado)
+	{
+		imprimir("usted acertó");
+	}
+
+	else{
+		imprimir("usted erró, el numero pensado era: " + numeroPensado);
+	}
+
+</script>
